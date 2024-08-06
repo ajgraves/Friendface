5b161d7f18667b352efc507789ffeb0a322662de
Aaron Graves <ajgraves@ajgraves.com>
2024-08-06 15:32:32 -0500

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Aaron Graves on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
