35f891b436a2951cd36d0567743532a47047cbf6
Aaron Graves <ajgraves@ajgraves.com>
2024-08-07 23:24:54 -0500

    Completed day 61, admittedly with a bit of help from: https://busayoajide.medium.com/how-to-build-a-swiftui-app-with-json-urlsession-and-swiftdata-d02c881236db

--- Friendface/ContentView.swift
+++ Friendface/ContentView.swift
@@ -5,10 +5,15 @@
 //  Created by Aaron Graves on 8/6/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-    @State private var users = [User]()
+    @Environment(\.modelContext) var modelContext
+    //@State private var users = [User]()
+    @Query(sort: [
+        SortDescriptor(\User.name)
+    ]) var users: [User]
     
     var body: some View {
         NavigationStack {
@@ -26,24 +31,22 @@ struct ContentView: View {
             }
             .navigationTitle("Friendface")
         }
-        .onAppear(perform: {
-            Task {
-                //users = (try? await loadData()) ?? []
-                await loadData()
-            }
-        })
+        .task {
+            //users = (try? await loadData()) ?? []
+            await loadData()
+        }
     }
     
     func loadData() async {
-        if !users.isEmpty { return } // If we alraeady have the data, then stop
+        /*if !users.isEmpty { return } // If we alraeady have the data, then stop
         
-        // We need to get data
-        let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+        // We need to get data*/
+        //let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
         //var request = URLRequest(url: url)
         //request.setValue("application/json", forHTTPHeaderField: "Content-type")
         //request.httpMethod = "GET"
         
-        do {
+        /*do {
             //let data = try await URLSession.shared.download(for: request)
             let (data, _) = try await URLSession.shared.data(from: url)
             
@@ -51,15 +54,38 @@ struct ContentView: View {
             decoder.dateDecodingStrategy = .iso8601
             let decodedData: [User] = try decoder.decode([User].self, from: data)
             //return decodedData
-            users = decodedData
+            //users = decodedData
+            users.insert(decodedData)
             
             // Sort by alphabetical
             // Sort by name, easy
-            users.sort { $0.name < $1.name }
+            //users.sort { $0.name < $1.name }
             
         } catch {
             print("Download failed: \(error)")
             return //[]
+        }*/
+        
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            fatalError("Invalid URL.")
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if users.isEmpty {
+                guard let decodedResponse = try? decoder.decode([User].self, from: data) else {
+                    fatalError("Failed to decode data.")
+                }
+                try? modelContext.delete(model: User.self)
+                for user in decodedResponse {
+                    modelContext.insert(user)
+                }
+            }
+        } catch {
+            fatalError("Failed to decode data.")
         }
     }
 }
--- Friendface/Friend.swift
+++ Friendface/Friend.swift
@@ -5,13 +5,29 @@
 //  Created by Aaron Graves on 8/6/24.
 //
 
+import SwiftData
 import Foundation
 
-struct Friend: Codable, Identifiable {
+@Model
+class Friend: Codable, Identifiable {
     enum CodingKeys: String, CodingKey {
         case id, name
     }
     
     var id: UUID = UUID()
     var name: String = ""
+    
+    
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.id, forKey: .id)
+        try container.encode(self.name, forKey: .name)
+    }
 }
